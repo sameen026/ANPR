06ec31e3e805493da1da5215a1cd1a35c747d862
sameen026 <bitf16m026@pucit.edu.pk>
2020-06-12 00:16:33 +0500

    Bugs Resolved

--- src/main/java/Recognizer.java
+++ src/main/java/Recognizer.java
@@ -101,7 +101,7 @@ public class Recognizer extends JFrame {
         panel3.add(picLabel);
 
         sp2= new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        sp2.setResizeWeight(0.7);
+        sp2.setResizeWeight(0.6);
         sp2.setEnabled(false);
         sp2.setDividerSize(5);
 
@@ -128,92 +128,32 @@ public class Recognizer extends JFrame {
         jFrame.setResizable(false);
         jFrame.addKeyListener(new MKeyListener() );
         jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(0);
     }
 
     // change the parameters of this function according to the labels you gonna use...currently using dummy data for extra labels
     public void populateJFrame(String pn,String vt,Image image) throws IOException{
-        panel1 = new JPanel();
-        panel1.setLayout(new BoxLayout(panel1,BoxLayout.Y_AXIS));
-        panel1.setBackground(Color.decode("#bbd6f2"));
-        plateNumberLabel=new JLabel("License Plate Number:");
-        plateNumberLabel.setFont(new Font("Serif", Font.BOLD, 22));
-        plateNumberLabel.setForeground(Color.decode("#0b3b6e"));
-        plateNumber.setText(pn);
-        panel1.add(plateNumber);
+
         plateNumber.setFont(new Font("Serif", Font.PLAIN, 22));
+        plateNumber.setText(pn);
 
-        vehicleTypeLabel=new JLabel("Vehicle Type:");
-        vehicleTypeLabel.setFont(new Font("Serif", Font.BOLD, 22));
-        vehicleTypeLabel.setForeground(Color.decode("#0b3b6e"));
-        vehicleType=new JLabel(vt);
         vehicleType.setFont(new Font("Serif", Font.PLAIN, 22));
+        vehicleType.setText(vt);
 
-        lastParkedVehicleLabel=new JLabel("Last Parked Vehicle:");
-        lastParkedVehicleLabel.setFont(new Font("Serif", Font.BOLD, 22));
-        lastParkedVehicleLabel.setForeground(Color.decode("#0b3b6e"));
-        lastParkedVehicle=new JLabel("c");
         lastParkedVehicle.setFont(new Font("Serif", Font.PLAIN, 22));
+        lastParkedVehicle.setText("c");
 
-        statusLabel=new JLabel("Status:");
-        statusLabel.setFont(new Font("Serif", Font.BOLD, 22));
-        statusLabel.setForeground(Color.decode("#0b3b6e"));
-        status=new JLabel("d");
         status.setFont(new Font("Serif", Font.PLAIN, 22));
+        status.setText("d");
 
-        extraLabel=new JLabel("Extra:");
-        extraLabel.setFont(new Font("Serif", Font.BOLD, 22));
-        extraLabel.setForeground(Color.decode("#0b3b6e"));
-        extra=new JLabel("e");
         extra.setFont(new Font("Serif", Font.PLAIN, 22));
+        extra.setText("e");
 
-        panel1.add(plateNumberLabel);
-        panel1.add(plateNumber);
-        panel1.add(vehicleTypeLabel);
-        panel1.add(vehicleType);
-        panel1.add(lastParkedVehicleLabel);
-        panel1.add(lastParkedVehicle);
-        panel1.add(statusLabel);
-        panel1.add(status);
-        panel1.add(extraLabel);
-        panel1.add(extra);
-        panel1.setVisible(true);
-
-
-        panel3=new JPanel();
-        panel3.setBackground(Color.decode("#bbd6f2"));
-        picLabel=new JLabel();
-        picLabel.setSize(250,300);
+        picLabel.setSize(300,250);
         Image dimg = image.getScaledInstance(picLabel.getWidth(), picLabel.getHeight(),
                 Image.SCALE_SMOOTH);
         ImageIcon imageIcon = new ImageIcon(dimg);
         picLabel.setIcon(imageIcon);
-
-        panel3.add(picLabel);
-        panel3.setVisible(true);
-
-
-        sp2= new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        sp2.setResizeWeight(0.9);
-        sp2.setEnabled(false);
-        sp2.setDividerSize(5);
-
-        sp2.add(panel1);
-        sp2.add(panel3);
-
-        panel2 = new JPanel();
-        panel2.setBackground(Color.decode("#bbd6f2"));
-
-        sp1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-        sp1.setResizeWeight(0.1);
-        sp1.setEnabled(false);
-        sp1.setDividerSize(5);
-
-        sp1.add(panel2);
-        sp1.add(sp2);
-        jFrame.add(sp1, BorderLayout.CENTER);
-        jFrame.setSize(800, 700);
-        jFrame.setVisible(true);
-        jFrame.setLocationRelativeTo(null);
     }
 
     public void getImage() throws IOException {
@@ -221,7 +161,7 @@ public class Recognizer extends JFrame {
         ImageIO.write(webcam.getImage(),"JPG",new File("firstCapture.jpg"));
     }
 
-    class MKeyListener extends KeyAdapter {
+    class MKeyListener extends KeyAdapter   {
 
         @Override
         public void keyPressed(KeyEvent event){
@@ -229,9 +169,9 @@ public class Recognizer extends JFrame {
             if (event.getKeyCode() == KeyEvent.VK_SPACE) {
                 try {
                     getImage();
-                    String result=JSONParser(numberPlateDigitization());
-                    String[] arr = result.split(",");
-                    populateJFrame(arr[0],arr[1], new ImageIcon("C:\\Users\\Sameen\\IdeaProjects\\untitled2\\firstCapture.jpg").getImage());
+                    MyRunnable myRunnable = new MyRunnable();
+                    Thread t = new Thread(myRunnable);
+                    t.start();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -297,6 +237,19 @@ public class Recognizer extends JFrame {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+        }
+    }
+
+    public class MyRunnable implements Runnable {
+
+        public void run() {
+            try {
+                String result=JSONParser(numberPlateDigitization());
+                String[] arr = result.split(",");
+                populateJFrame(arr[0],arr[1], new ImageIcon("C:\\Users\\Sameen\\IdeaProjects\\untitled2\\firstCapture.jpg").getImage());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
